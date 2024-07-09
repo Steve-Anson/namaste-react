1380e04e2d8b2a7297c4e297e542383bd0c1e45d
Steve-Anson <steveanson12@gmail.com>
2024-07-09 17:34:27 +0530

    Assignment 2

---
+++ Assignments/Episode 2/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement(
+  "h1",
+  { className: "heading" },
+  "Hello World from React"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
