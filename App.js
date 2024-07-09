893fb48b55099ee736ae5b080d5435925d5d187d
Steve-Anson <steveanson12@gmail.com>
2024-07-09 14:58:33 +0530

    Production build

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
-// Nested Elements
-// const heading = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },
-//     React.createElement("h1", {}, "I'm h1 tag")
-//   )
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// Multiple childs
-// const heading = React.createElement("div", { id: "child" }, [
-//   React.createElement("h1", {}, "I'm h1 tag"),
-//   React.createElement("h2", {}, "I'm h2 tag"),
-// ]);
 const heading = React.createElement("h1", {}, "Hello World from React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
