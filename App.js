83c9c481d8135eea1e47ab848128b1f643818f65
Steve-Anson <steveanson12@gmail.com>
2024-07-11 17:17:07 +0530

    Insert JS code inside JSX/Component

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Title = () => <h1>Namaste React</h1>;
+
 const HeadingComponent = () => (
   <div id="container">
+    <Title />
     <h1 className="heading">Hello World from Functional Component</h1>
   </div>
 );
